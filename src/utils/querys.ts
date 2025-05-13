18cf9818b66dd9b93649c160af48532bf03f3af8
Tiago Navarro <navarrotiago194@gmail.com>
2025-05-13 12:41:46 -0300

    update

--- src/utils/querys.ts
+++ src/utils/querys.ts
@@ -1,7 +1,8 @@
 export const GetProductByIdQuery =
-  "http://localhost:1337/api/products?fields[0]=name&fields[1]=price&fields[2]=description&populate[stock][fields][0]=quantity&populate[stock][populate][images][fields][0]=url&populate[stock][populate][size_stock][fields][0]=name&populate[stock][fields][1]=color&filters[productID][$contains]=";
+  "https://upbeat-boat-384ceaadbe.strapiapp.com/api/products?fields[0]=name&fields[1]=price&fields[2]=description&populate[stock][fields][0]=quantity&populate[stock][populate][images][fields][0]=url&populate[stock][populate][size_stock][fields][0]=name&populate[stock][fields][1]=color&filters[productID][$contains]=";
 
 export const GetProductsQuery =
-  "http://localhost:1337/api/products?fields[0]=name&fields[1]=price&fields[2]=productID&populate[stock][populate][images][fields][0]=url";
+  "https://upbeat-boat-384ceaadbe.strapiapp.com/api/products?fields[0]=name&fields[1]=price&fields[2]=productID&populate[stock][populate][images][fields][0]=url";
 
-export const GetCategoriesQuery = "http://localhost:1337/api/categories";
+export const GetCategoriesQuery =
+  "https://upbeat-boat-384ceaadbe.strapiapp.com/api/categories";
